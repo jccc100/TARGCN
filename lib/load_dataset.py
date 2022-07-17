7c6a8bcbd7805ffd77813e2de77d614c4ea622bd
csust-sonie <48102706+csust-sonie@users.noreply.github.com>
2022-07-17 19:24:03 +0800

    Add files via upload
    commit code

---
+++ lib/load_dataset.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+
+def load_st_dataset(dataset):
+    #output B, N, D
+    if dataset == 'PEMSD4':
+        data_path = os.path.join('../data/PEMSD4/pems04.npz')
+        data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
+    elif dataset == 'PEMSD8':
+        data_path = os.path.join('../data/PEMSD8/pems08.npz')
+        data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
+    elif dataset == 'PEMS03':
+        data_path = os.path.join('../data/PEMS03/pems03.npz')
+        data = np.load(data_path)['data'][:, :, :]  #onley the first dimension, traffic flow data
+    elif dataset == 'PEMSD7':
+        data_path = os.path.join('../data/PEMSD7/PEMS07.npz')
+        data = np.load(data_path)['data'][:, :, :]  #onley the first dimension, traffic flow data
+    else:
+        raise ValueError
+    if len(data.shape) == 2:
+        data = np.expand_dims(data, axis=-1)
+    print('Load %s Dataset shaped: ' % dataset, data.shape, data.max(), data.min(), data.mean(), np.median(data))
+    return data
